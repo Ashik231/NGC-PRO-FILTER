e1eb6c6466012a6ff645dd6e20d9d0b55eff6598
ᴀꜱʜɪᴋ ᴍʜᴅ <85797232+Ashik231@users.noreply.github.com>
2022-07-10 14:42:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master  https://github.com/VIBEFLiX/ngc_muthmani
-cd /ngc_muthmani
+git clone -b master https://github.com/Ashik231/NGC-PRO-FILTER.git /NGC-PRO-FILTER
+cd /NGC-PRO-FILTER
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
